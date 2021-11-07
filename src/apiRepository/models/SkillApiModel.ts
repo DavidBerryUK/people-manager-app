fdf437e7324e36fbb898247d89769c570cf3a630
David Berry <DavidBerryUK@hotmail.co.uk>
2021-11-07 20:32:17 +0000

    skills

--- src/apiRepository/models/SkillApiModel.ts
+++ src/apiRepository/models/SkillApiModel.ts
@@ -3,11 +3,13 @@ import SkillLevelApiModel from "./SkillLevelApiModel";
 export default class SkillApiModel {
   id: number;
   name: string;
+  iconName: string;
   people: Array<SkillLevelApiModel>;
 
-  constructor(id: number, name: string) {
+  constructor(id: number, name: string, iconName: string) {
     this.id = id;
     this.name = name;
+    this.iconName = iconName;
     this.people = new Array<SkillLevelApiModel>();
   }
 }
