1d981ac8ed3e9b89f58ec9e3f146d0d268ba0ec4
David Berry <DavidBerryUK@hotmail.co.uk>
2021-11-09 16:32:26 +0000

    stuff

--- src/components/widgetLayout/listLayout/LayoutList.tsx
+++ src/components/widgetLayout/listLayout/LayoutList.tsx
@@ -1,31 +1,6 @@
-import { useLocation } from "react-router";
-import React, { useMemo } from "react";
-//import { UseListDetailContext } from "../../../contexts/ListDetailContext.tsx/ListDetailContext";
-//import ListDetailUrlManager from "../../../services/urlManagers/ListDetailUrlManger";
 import "./Styles.scss";
 
 const LayoutList: React.FC = (props) => {
-  const location = useLocation();
-  //  const { state } = UseListDetailContext();
-
-  // detect when brower location has changed
-  //
-  useMemo(() => {
-    console.log("****************** LayoutList Component ******************");
-    console.log(`* location has changed to ${location.pathname}`);
-    console.log("**********************************************************");
-    console.log("");
-  }, [location]);
-
-  // detect when navigation state in context has changed
-  //
-  // useMemo(() => {
-  //   console.log("****************** LayoutList Component ******************");
-  //   console.log("list state changed");
-  //   const params = ListDetailUrlManager.createUrlParams(null, state.detailView.viewType, state.detailView.detailKey);
-  //   console.log(params);
-  // }, [state]);
-
   return <div className="layout-list">{props.children}</div>;
 };
 
--- src/components/widgetsDataLists/peopleTable/PeopleTableWidget.tsx
+++ src/components/widgetsDataLists/peopleTable/PeopleTableWidget.tsx
@@ -6,47 +6,41 @@ import PaginationWidget from "../../widgetsUI/pagination/PaginationWidget";
 import PeopleRowWidget from "./PeopleRowWidget";
 import PeopleTableHeader from "./PeopleTableHeader";
 import React, { useMemo } from "react";
-import { UseListDetailContext } from "../../../contexts/ListDetailContext.tsx/ListDetailContext";
 import ListDetailUrlManager from "../../../services/urlManagers/ListDetailUrlManger";
+import { UseListDetailContext } from "../../../contexts/ListDetailContext.tsx/ListDetailContext";
 import { useHistory, useLocation } from "react-router";
 
 const PeopleTableWidget: React.FC = () => {
   const { state: peopleState, dispatch: peopleDispatch } = UsePeopleContext();
-  const { state: ListDetailState } = UseListDetailContext();
-  const location = useLocation();
+  const { state: listDetailContext } = UseListDetailContext();
   const history = useHistory();
+  const location = useLocation();
 
-  //
-  // Get the data from the repository
-  //
+  // detect url change
+  useMemo(() => {
+    console.log(`Url Params Changed:${location.search}`);
+  }, [location.search]);
+
+  // Get data when pagination state changes
   useMemo(async () => {
     console.log("****************** People Table [GET DATA] - get data ******************");
     const apiRepositoryPeopleList = new ApiRepositoryPeopleList();
     const peopleList = await apiRepositoryPeopleList.getPeopleAsync(peopleState.pagination.sortColumn, peopleState.pagination.sortDirection, peopleState.pagination.pageNumber, peopleState.pagination.rowsPerPage);
-
     // update context with data
-    //
+
     peopleDispatch(new CommandPeopleListSet(peopleList.data, peopleList.rowsPerPage, peopleList.totalPages, peopleList.totalRows));
   }, [peopleDispatch, peopleState.pagination]);
-
-  //
-  // Update URL
-  //
-  useMemo(async () => {
-    const params = ListDetailUrlManager.createUrlParams(peopleState.pagination, ListDetailState.detailView);
-    console.log("****************** People Table [Update URL] ******************");
-    if (location.search !== params) {
-      history.push({ pathname: location.pathname, search: params });
-    }
-  }, [peopleState.pagination, ListDetailState.detailView, location, history]);
 
   //
   // Event Handlers
   //
   const handleOnPageChangeEvent = (pageNo: number) => {
     // update context with new page number
-    //
     peopleDispatch(new CommandPageNumberSet(pageNo));
+
+    // set history
+    var params = ListDetailUrlManager.createUrlParams(peopleState.pagination, listDetailContext.detailView);
+    history.push({ pathname: location.pathname, search: params });
   };
 
   return (
