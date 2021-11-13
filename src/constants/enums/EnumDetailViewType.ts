e3e92e44456a40a7e9f1b143d54480cc415eb5e2
David Berry <DavidBerryUK@hotmail.co.uk>
2021-11-13 17:12:14 +0000

    fix enum converts

--- src/constants/enums/EnumDetailViewType.ts
+++ src/constants/enums/EnumDetailViewType.ts
@@ -1,7 +1,58 @@
 export enum EnumDetailViewType {
-    none = "none",
-    person = "person",
-    role = "role",
-    skill = "skill",
-    team = "team",
+  none,
+  person,
+  role,
+  skill,
+  team,
+}
+
+const noneText = "none";
+const personText = "person";
+const roleText = "role";
+const skillText = "skill";
+const teamText = "team";
+
+// may be a better way of doing this, but could not
+// get string->enum to compare with another enum correctly
+export default class EnumDetailViewTypeConvert {
+  static toEnum(value: string): EnumDetailViewType {
+    if (value === undefined || value === null) {
+      return EnumDetailViewType.none;
+    }
+
+    switch (value.toLocaleLowerCase().trim()) {
+      case noneText:
+        return EnumDetailViewType.none;
+      case personText:
+        return EnumDetailViewType.person;
+      case roleText:
+        return EnumDetailViewType.role;
+      case skillText:
+        return EnumDetailViewType.skill;
+      case teamText:
+        return EnumDetailViewType.team;
+    }
+
+    return EnumDetailViewType.none;
+  }
+
+  static toString(value: EnumDetailViewType): string {
+    if (value === undefined || value === null) {
+      return noneText;
+    }
+
+    switch (value) {
+      case EnumDetailViewType.none:
+        return noneText;
+      case EnumDetailViewType.person:
+        return personText;
+      case EnumDetailViewType.role:
+        return roleText;
+      case EnumDetailViewType.skill:
+        return skillText;
+      case EnumDetailViewType.team:
+        return teamText;
+    }
+    return noneText;
+  }
 }
--- src/services/urlManagers/models/UrlParametersModel.ts
+++ src/services/urlManagers/models/UrlParametersModel.ts
@@ -1,64 +1,59 @@
-import { EnumDetailViewType } from "../../../constants/enums/EnumDetailViewType";
+import EnumDetailViewTypeConvert, { EnumDetailViewType } from "../../../constants/enums/EnumDetailViewType";
 import EnumSortColumnConvert, { EnumSortColumn } from "../../../constants/enums/EnumSortColumn";
 import EnumSortDirectionConvert, { EnumSortDirection } from "../../../constants/enums/EnumSortDirection";
-import UrlParameterNames from "../constants/UrlParamterNames";
+import UrlParameterNames from "../constants/UrlParameterNames";
 
 //
-// strongly typed model that represents values in a 
+// strongly typed model that represents values in a
 // url parameter string. the values are provided to this model
 // as a dictionary in the constructor
 //
 export default class UrlModel {
+  private dictionary: { [name: string]: string } = {};
 
-    private dictionary: { [name: string]: string } = {};
+  readonly pageNumber: number;
+  readonly rowsPerPage: number;
+  readonly sortColumn: EnumSortColumn;
+  readonly sortDirection: EnumSortDirection;
+  readonly detailViewType: EnumDetailViewType;
+  readonly detailKey: number;
 
-    readonly pageNumber: number
-    readonly rowsPerPage: number
-    readonly sortColumn: EnumSortColumn;
-    readonly sortDirection: EnumSortDirection;
-    readonly detailViewType: EnumDetailViewType;
-    readonly detailKey: number;
+  //
+  // Constructor
+  //
+  constructor(dictionary: { [name: string]: string } = {}) {
+    this.dictionary = dictionary;
+    this.pageNumber = this.getNumberForKey(UrlParameterNames.pageNumber, 1);
+    this.rowsPerPage = this.getNumberForKey(UrlParameterNames.rowsPerPage, 10);
+    this.detailKey = this.getNumberForKey(UrlParameterNames.detailViewKey, 0);
+    this.sortColumn = this.evaluateSortColumn();
+    this.sortDirection = this.evaluateSortDirection();
+    this.detailViewType = this.evaluateDetailType();
+  }
 
-    //
-    // Constructor
-    //
-    constructor(dictionary: { [name: string]: string } = {}) {
-        this.dictionary = dictionary;
-        this.pageNumber = this.getNumberForKey(UrlParameterNames.pageNumber, 1);
-        this.rowsPerPage = this.getNumberForKey(UrlParameterNames.rowsPerPage, 10);
-        this.detailKey = this.getNumberForKey(UrlParameterNames.detailViewKey, 0);
-        this.sortColumn = this.evaluateSortColumn();
-        this.sortDirection = this.evaluateSortDirection();
-        this.detailViewType = this.evaluateDetailType();
+  private getNumberForKey(key: string, defaultValue: number): number {
+    var value = this.dictionary[key];
+    if (value === undefined) {
+      return defaultValue;
     }
+    return Number(value);
+  }
 
-    private getNumberForKey(key: string, defaultValue: number): number {
-        var value = this.dictionary[key];
-        if (value === undefined) {
-            return defaultValue;
-        }
-        return Number(value);
-    }
+  private evaluateSortDirection(): EnumSortDirection {
+    var textValue = this.dictionary[UrlParameterNames.sortDirection];
+    var enumValue = EnumSortDirectionConvert.toEnum(textValue);
+    return enumValue;
+  }
 
-    private evaluateSortDirection(): EnumSortDirection {
-        var textValue = this.dictionary[UrlParameterNames.sortDirection];
-        var enumValue = EnumSortDirectionConvert.toEnum(textValue);
-        return enumValue;
-    }
+  private evaluateSortColumn(): EnumSortColumn {
+    var textValue = this.dictionary[UrlParameterNames.sortColumn];
+    var enumValue = EnumSortColumnConvert.toEnum(textValue);
+    return enumValue;
+  }
 
-    private evaluateSortColumn(): EnumSortColumn {
-        var textValue = this.dictionary[UrlParameterNames.sortColumn];
-        var enumValue = EnumSortColumnConvert.toEnum(textValue);
-        return enumValue;
-    }
-
-    private evaluateDetailType(): EnumDetailViewType {
-        var value = this.dictionary[UrlParameterNames.detailViewType];
-        if (value === undefined) {
-            return EnumDetailViewType.none;
-        }
-        return value as EnumDetailViewType;
-    }
-
-
+  private evaluateDetailType(): EnumDetailViewType {
+    var textValue = this.dictionary[UrlParameterNames.detailViewType];
+    var enumValue = EnumDetailViewTypeConvert.toEnum(textValue);
+    return enumValue;
+  }
 }
